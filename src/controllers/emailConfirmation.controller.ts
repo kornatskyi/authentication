4b5588cc9ed25198e019768980c484a78d8b252e
Bohdan <bohdan.kornatskyi@gmail.com>
2021-09-20 20:28:49 -0700

    email confirmation check

--- src/controllers/emailConfirmation.controller.ts
+++ src/controllers/emailConfirmation.controller.ts
@@ -2,35 +2,37 @@ import { Request, Response } from "express";
 import User from "../models/user.model";
 const jwt = require("jsonwebtoken");
 
-// //Sending mail with email confirmation link
-// sendEmail().catch((err) => {
-//     console.log(err);
-//   });
-
 export function isConfirmed(req: Request, res: Response) {
-  User.getEmailConfirmationStatus(req.body.email, (err: Error, result: any) => {
-    if (err) {
-      if (err.message === "not_found") {
-        res.status(404).send({
-          message: `Not found  user with ${req.params.customerId} email`,
-        });
-      } else {
-        res.status(500).send({
-          message:
-            "Error retrieving the confirmation status of " +
-            req.params.customerId,
-        });
+  if (req.session.user) {
+    User.getEmailConfirmationStatus(
+      req.session.user.email,
+      (err: Error, result: any) => {
+        if (err) {
+          if (err.message === "not_found") {
+            res.status(404).send({
+              message: `Not found  user with ${req.params.customerId} email`,
+            });
+          } else {
+            res.status(500).send({
+              message:
+                "Error retrieving the confirmation status of " +
+                req.params.customerId,
+            });
+          }
+        } else {
+          if (result.confirmed !== 0) {
+            res.status(200);
+            res.send(true);
+          } else {
+            res.status(200);
+            res.send(false);
+          }
+        }
       }
-    } else {
-      if (result.confirmed !== 0) {
-        res.status(200);
-        res.send(true);
-      } else {
-        res.status(200);
-        res.send(false);
-      }
-    }
-  });
+    );
+  } else {
+    res.status(401).send("Session doesn't exist");
+  }
 }
 
 export function confirm(req: Request, res: Response) {
