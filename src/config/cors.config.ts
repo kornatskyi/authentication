d4bb6232eec511e519e620fbb8f93abd6c6b12ae
Bohdan <bohdan.kornatskyi@gmail.com>
2021-09-30 15:13:59 -0700

    cros settings

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,5 +1,5 @@
 export default {
-  origin: "http://localhost:5000",
+  origin: true,
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
