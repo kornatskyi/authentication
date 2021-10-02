d6f5c46e3fb6a5bb1267a019581a0dd1aa7eed72
Bohdan <bohdan.kornatskyi@gmail.com>
2021-10-01 17:36:48 -0700

    remove prod changes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
 import app from "./app";
 
-console.log("Some logs to consoooole!!!!");
-
-setInterval(() => {
-  console.log("Some log on:");
-
-  console.log(new Date());
-}, 1000);
-
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
